1c1a6d2a3c092f6ae5846d6938be21fa33d59061
ajaragoneses <aj.aragoneses@gmail.com>
2015-04-10 12:46:08 +0200

    [Team] Creado el test para comprobar que se puede mover la carta al foundation

--- MWL.P2.TDD/src/test/WasteToFoundationMoveTest.java
+++ MWL.P2.TDD/src/test/WasteToFoundationMoveTest.java
@@ -2,23 +2,51 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class WasteToFoundationMoveTest {
 
 	public Waste waste;
+	public Deck deck;
+	public Foundations foundations;
 	
 	@BeforeClass
 	public void init(){
-		Waste waste = new Waste();
-		Deck deck = new Deck();
-		deck.moveToWaste();
+		waste = new Waste();
+		deck = new Deck();
+		foundations = new Foundations();
+		
+		Card card_1 = new Card(Color.ROJO, Palo.ROMBOS, Valor.Q);
+		Card card_2 = new Card(Color.NEGRO, Palo.PICAS, Valor.3);
+		Card card_3 = new Card(Color.ROJO, Palo.CORAZONES, Valor.7);
+		Card card_4 = new Card(Color.NEGRO, Palo.PICAS, Valor.K);
+		Card card_5 = new Card(Color.ROJO, Palo.ROMBOS, Valor.4);
+		
+		foundations.get(0).setTopCard(card_1);
+		foundations.get(1).setTopCard(card_2);
+		foundations.get(2).setTopCard(card_3);
+		foundations.get(3).setTopCard(card_4);
+		waste.setTopCard(card_5);
 	}
 	
 	@Test
 	public void WasteNotEmptyTest() {
 		assertTrue(waste.isWasteGetCardPermited());
+	}
+	
+	@Test
+	public void MoveToFoundationPermited(){
+		Foundation foundation = foundations.get(0);
+		assertFalse(foundation.isMoveToFoundationPermited(waste.getTopCard()));
+		foundation = foundations.get(1);
+		assertTrue(foundation.isMoveToFoundationPermited(waste.getTopCard()));
+		foundation = foundations.get(2);
+		assertFalse(foundation.isMoveToFoundationPermited(waste.getTopCard()));
+		foundation = foundations.get(3);
+		assertFalse(foundation.isMoveToFoundationPermited(waste.getTopCard()));
 	}
 
 }

cfa6e92a480802fa3b68e890fc8495fea6b4bc85
Alvaro <Alvaro@138.4.192.94>
2015-04-10 18:05:04 +0200

    [Test] Comprobamos que Waste no es vacío

--- MWL.P2.TDD/src/Klondike/Waste.java
+++ MWL.P2.TDD/src/Klondike/Waste.java
@@ -27,4 +27,9 @@ public class Waste {
 		
 	}
 
+	public void addCardOnTop(Card card_5) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- MWL.P2.TDD/src/test/WasteToDeckMoveTest.java
+++ MWL.P2.TDD/src/test/WasteToDeckMoveTest.java
@@ -2,13 +2,44 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import Klondike.Card;
+import Klondike.Color;
+import Klondike.Deck;
+import Klondike.Foundations;
+import Klondike.Palo;
+import Klondike.Valor;
+import Klondike.Waste;
 
 public class WasteToDeckMoveTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public Waste waste;
+	public Deck deck;
+	public Foundations foundations;
+	
+	@Before
+	public void init(){
+		waste = new Waste();
+		deck = new Deck();
+		
+		Card card_1 = new Card(Color.ROJO, Palo.ROMBOS, Valor.Q);
+		Card card_2 = new Card(Color.NEGRO, Palo.PICAS, Valor.TRES);
+		Card card_3 = new Card(Color.ROJO, Palo.CORAZONES, Valor.SIETE);
+		Card card_4 = new Card(Color.NEGRO, Palo.PICAS, Valor.K);
+		Card card_5 = new Card(Color.ROJO, Palo.ROMBOS, Valor.CUATRO);
+		
+		waste.addCardOnTop(card_5);
+		waste.addCardOnTop(card_4);
+		waste.addCardOnTop(card_3);
+		waste.addCardOnTop(card_2);
+		waste.addCardOnTop(card_1);
 	}
-
+	
+	
+	@Test
+	public void WasteNotEmptyTest() {
+		assertTrue(waste.isWasteGetCardPermited());
+	}
 }

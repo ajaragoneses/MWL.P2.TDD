d863a4dd1f4e72dba75213b25ea339503ab4796d
ajaragoneses <aj.aragoneses@gmail.com>
2015-04-10 12:03:28 +0200

    [Test] Creado los test para el caso de uso mover de Waste a Foundation

---
+++ MWL.P2.TDD/src/test/WasteToFoundationMoveTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class WasteToFoundationMoveTest {
+
+	@BeforeClass
+	public void init(){
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
